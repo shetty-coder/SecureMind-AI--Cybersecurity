f02fb313ba0b28556a0989b74f5db1a070d27d5e
shetty-coder <thejaswinishetty324@gmail.com>
2026-04-29 08:47:24 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,7 +0,0 @@
-# =========================
-# backend/services/risk_score.py
-# =========================
-def calculate_risk(prediction):
-    if prediction == "Attack Detected":
-        return 95
-    return 10
